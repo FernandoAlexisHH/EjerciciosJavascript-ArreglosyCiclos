5baa8a1ec4e071ca7c11c31f8b57e7e0c6acff54
FernandoAlexisHH <fer_guitarrista@hotmail.com>
2025-02-05 02:11:54 -0600

    Resolviendo ordenamiento de burbuja

--- main.js
+++ main.js
@@ -125,3 +125,19 @@ console.log("Estado final del array:", people);
 // Write a function that performs the bubble algorithm.
 // Input [3, 6, 12, 5, 100, 1]
 // Output [1, 3, 5, 6, 12, 100]
+let arrBubble = [3, 6, 12, 5, 100, 1];
+const bubbleAlgorithm = (arr) => {
+  let aux;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        aux = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = aux;
+      }
+    }
+  }
+  return arr;
+};
+
+console.log(bubbleAlgorithm(arrBubble));
