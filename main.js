2fce236fe67ad1f2fa24a6dd9fcc6e2c8a9850d5
FernandoAlexisHH <fer_guitarrista@hotmail.com>
2025-02-04 21:55:05 -0600

    Añadir función para encontrar cursos comunes entre dos estudiantes

--- main.js
+++ main.js
@@ -60,6 +60,25 @@ console.log(sumAndProduct([1, 2, 3, 4]));
 // let student1Courses = ['Math', 'English', 'Programming'];
 // let student2Courses = ['Geography', 'Spanish', 'Programming'];
 // Create a program that loops over the 2 arrays; if there are any common courses print them out to the console.
+let student1Courses = ["Math", "English", "Programming", "hola", "Science"];
+let student2Courses = ["Geography", "Spanish", "Programming", "Science"];
+
+const commonCourses = (course1, course2) => {
+  let equalCourses = [];
+  for (let i = 0; i < course1.length; i++) {
+    for (let j = 0; j < course2.length; j++) {
+      if (course1[i] === course2[j]) equalCourses.push(course1[i]);
+    }
+  }
+  return equalCourses;
+};
+//Con filter e includes
+let commonCoursesArray = student1Courses.filter((course) =>
+  student2Courses.includes(course)
+);
+
+console.log(commonCoursesArray);
+console.log(commonCourses(student1Courses, student2Courses));
 
 // Exercise #5
 // For each of the exercises below, assume you are starting with the following people array.
